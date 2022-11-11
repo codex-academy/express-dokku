5bcf2a744d54847cd3a6b47c44ee90ab55e4da0d
Thabang Gideon Magaola <gideon877@live.com>
2022-11-11 15:17:49 +0200

    changed route returned values

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const app = express();
 
 const PORT = process.env.PORT || 4007;
 
-app.get('/', (req, res) => res.send('Hello!'));
+app.get('/', (req, res) => res.send({ message: 'Hello!', status: 200}));
 
 app.listen(PORT, () => console.log(`App started : ${PORT}`))
